224130791ee8f2f65bfe4ce29f170a26e023d351
Marat Charlaganov <charlaganov@gmail.com>
2013-08-20 16:49:35 +0200

    Indent using spaces

--- dump.py
+++ dump.py
@@ -5,10 +5,10 @@ from oaipmh.metadata import MetadataRegistry, oai_dc_reader
 easy_id = Namespace('https://easy.dans.knaw.nl/ui/datasets/id/easy-dataset:')
 
 def easy_url(oai_id):
-	namespace, dataset = oai_id.rsplit(':', 1)
-	if namespace != 'oai:easy.dans.knaw.nl:easy-dataset':
-		raise(Exception("Unknown namespace: {0}".format(namespace)))
-	return easy_id[dataset]
+    namespace, dataset = oai_id.rsplit(':', 1)
+    if namespace != 'oai:easy.dans.knaw.nl:easy-dataset':
+        raise(Exception("Unknown namespace: {0}".format(namespace)))
+    return easy_id[dataset]
 
 registry = MetadataRegistry()
 registry.registerReader('oai_dc', oai_dc_reader)
@@ -18,13 +18,13 @@ graph.namespace_manager.bind('dc11', 'http://purl.org/dc/elements/1.1/')
 dc11 = Namespace('http://purl.org/dc/elements/1.1/')
 # max_count = 30000
 for count, (header, metadata, _) in enumerate(client.listRecords(metadataPrefix='oai_dc')):
-	# if count >= max_count:
-	# 	break
-	if metadata is not None:
-	    metadata_fields = metadata.getMap().iteritems()
-		s = easy_url(header.identifier())
-		for p, vv in metadata_fields:
-			for v in vv:
-				graph.add((s, dc11[p], Literal(v)))
+    # if count >= max_count:
+    #   break
+    if metadata is not None:
+        metadata_fields = metadata.getMap().iteritems()
+        s = easy_url(header.identifier())
+        for p, vv in metadata_fields:
+            for v in vv:
+                graph.add((s, dc11[p], Literal(v)))
 
 graph.serialize('easy-lod.nt', format='nt')
